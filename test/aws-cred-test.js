d5e45c7f413521ae4a0488cf317614e01ea3d3a7
Brian Hamman <hamman@gmail.com>
2012-06-09 13:45:46 -0400

    full test coverage

--- test/aws-cred-test.js
+++ test/aws-cred-test.js
@@ -18,6 +18,30 @@ module.exports = {
 	    test.equal(awscred.env, "donkey");
 	    test.done();
   	},
+  	'Reads config file': function(test) {
+	    awscred = new AWSCred({"config_path":example_path});
+	    test.equal(awscred.access_key, "YOUR_KEY_HERE (required)");
+	    test.equal(awscred.secret_access_key, "YOUR_SECRET_KEY_HERE (required)");
+	    test.equal(awscred.account_id, "YOUR_KEY_HERE (optional)");
+	    test.done();
+  	},
+  	'Throws an error if no conf file': function(test) {
+	    test.throws(function(){
+	    	awscred = new AWSCred({"config_path":"aws.donkey.conf"});
+	    });
+	    test.done();
+  	},
+  	'Calculates config_path correctly for production': function(test) {
+	    awscred = new AWSCred({"env": "production", "config_path":example_path});
+	    test.equal(awscred.derive_config_path(), "/etc/aws.conf");
+	    test.done();
+  	},
+   	'Calculates config_path correctly for other environments': function(test) {
+	    awscred = new AWSCred({"env": "donkey", "config_path":example_path});
+	    test.equal(awscred.derive_config_path(), "/etc/aws.donkey.conf");
+	    test.done();
+  	},
+
 }
 
 
