0ecc7a06b13a4395bf1efc79bbdebfcb8bf156f8
Brian Hamman <hamman@gmail.com>
2012-06-09 13:29:35 -0400

    basic tests pass

--- test/aws-cred-test.js
+++ test/aws-cred-test.js
@@ -1,1 +1,24 @@
-//todo
+AWSCred = require('./../lib/aws-cred');
+
+var example_path = __dirname+"/../lib/aws.example.conf";
+
+module.exports = {
+	'Can set the config path': function(test) {
+	    awscred = new AWSCred({"env": "donkey", "config_path":example_path});
+	    test.equal(awscred.config_path, example_path);
+	    test.done();
+  	},
+	'Loads Development Environment by Default': function(test) {
+	    awscred = new AWSCred({"config_path":example_path});
+	    test.equal(awscred.env, "development");
+	    test.done();
+  	},
+  	'Can set the environment': function(test) {
+	    awscred = new AWSCred({"env": "donkey", "config_path":example_path});
+	    test.equal(awscred.env, "donkey");
+	    test.done();
+  	},
+}
+
+
+
